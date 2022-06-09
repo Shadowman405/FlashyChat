44588b66b06942093b73ad5ecbc283f6efe70f0d
Maxim Mitin <maximus405@gmail.com>
2022-06-09 21:56:27 +0300

    adding Message model , working with chat VC ( tableview and cell)

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 import Firebase
 
 class ChatViewController: UIViewController {
+    
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Sosi pes"),
+        Message(sender: "UncleBogdan@bebra.ru", body: "Do you like what you see slave??"),
+        Message(sender: "Vansama@gmail.com", body: "Hoi-hoi-hoi , Im fckng cumming")
+    ]
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         
         navigationItem.hidesBackButton = true
         title = K.appName
@@ -32,5 +39,19 @@ class ChatViewController: UIViewController {
                     print("Error signing out: %@", signOutError)
                 }
         }
+    
+}
+
+extension ChatViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        
+        return cell
+    }
     
 }
