ef772de8e46895268599d8529b1e4c027443e234
Maxim Mitin <maximus405@gmail.com>
2022-06-13 22:56:34 +0300

    work with ChatVC , scrolling to bottom with every new message and when app is load for first time

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -96,6 +96,8 @@ extension ChatViewController: UITableViewDataSource, UITableViewDelegate {
                             
                             DispatchQueue.main.async {
                                 self.tableView.reloadData()
+                                let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                             }
                         }
                     }
