92528ab3376ef11614195fd44adae4a08815281e
Maxim Mitin <maximus405@gmail.com>
2022-06-13 23:08:20 +0300

    after reading docs need understand thet we need add super in view

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,6 +14,7 @@ class WelcomeViewController: UIViewController {
     @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = true
     }
      
@@ -25,6 +26,7 @@ class WelcomeViewController: UIViewController {
     }
     
     override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         navigationController?.isNavigationBarHidden = false
     }
     
